d542b7dd810534cfc28b7da8ab2f3da8a5989cbe
AKrzanecka <alic980314@gmail.com>
2019-12-09 09:50:59 +0100

    Dodanie zakładek na podstronie Add_animal.php

--- add_animal.php
+++ add_animal.php
@@ -1,7 +1,46 @@
 <?php
 include "php/class/Animal.php";
 include "php/class/DBConnection.php";
+?>
+    <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+            <a class="navbar-brand" href="#">aDogted</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarText">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="/schronisko/glowna.php">Glowna <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/schronisko/adoption.php">Do adopcji</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/schronisko/login.php">Panel użytkownika</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="container-fluid hero-container">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <h1 class="text-light display-1">Zaadoptuj przjaciela</h1>
+            </div>
+        </div>
+    </div>
+<div class="container-fluid animals pt-5 px-5">
+    <div class="row">
+        <div class="col-lg-12 text-center">
+            <h2>ZAADOPTUJ</h2>
+            <h1>Nasi podopieczni</h1>
+        </div>
+    </div>
+    <div class="row">
 
+<?php
 $animal = new Animal();
 
 //if ($user->getSession()===TRUE) {
@@ -137,3 +176,10 @@ if(isset($_POST['submit'])){
 <?php
 //include('templates/footer.php');
 ?>
+    </div>
+    <div class="col-lg-4"></div>
+    <div class="col-lg-4"></div>
+</div>
+    </div>
+    </body>
+</html>
