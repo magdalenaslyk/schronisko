8b119587d02371b7f7a66855ee2171a78dca608e
mackiewiczmichal <threemz69@gmail.com>
2019-12-11 19:24:07 +0100

    Dodanie nagłówków
    +działanie styli
    +dodanie działającego headera

--- templates/footer.php
+++ templates/footer.php
@@ -1,3 +1,15 @@
 <?php
 //TODO: Header template
 ?>
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+            <img src="/schronisko/img/site/logo.png">
+            </div>
+        </div>
+    </div>
+</footer>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</body>
+</html>
